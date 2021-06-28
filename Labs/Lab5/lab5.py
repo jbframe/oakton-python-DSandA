07c95eccf694fece766aba2ffa1e1d7cb91dcdff
jbframe <framejb@gmail.com>
2021-06-27 18:37:54 -0600

    Added time measurements.

--- Labs/Lab5/lab5.py
+++ Labs/Lab5/lab5.py
@@ -3,8 +3,8 @@ Name: John Frame, Student Number: B02291550, Date: 6/26/2021
 Lab # & Name: 5 - Searching and Sorting Techniques with Time Complexity
 Course: CSC 242 Python Data Structures
 """)
-
-# ----------------Sequential Search (without explicit iterations)------------------- #
+import datetime
+# -------------------Sequential Search (without explicit iterations)------------------- #
 # # search a list for particular elements
 
 # # declare a list of letters and numbers
@@ -25,12 +25,13 @@ Course: CSC 242 Python Data Structures
 # else :
 #     print ("item was NOT located")
 
-# # ---------------------Sequential Linear Search----------------------------------- #
+# ---------------------------Sequential Linear Search----------------------------------- #
 # implementation of a Linear Search
 # time complexity ( Order of Magnitude ) : O(n)
 
 def LinearSearchWithPrinting(sortedList, n):
     x = int(input("Enter the number to search: "))
+    tStamp1 = datetime.datetime.now()
     # Boolean Flag
     found = False
     for i in range(len(sortedList)) :
@@ -40,10 +41,14 @@ def LinearSearchWithPrinting(sortedList, n):
             break
         if (found == False) :
             print("%d is not in %dth position" % (x, i))
+    tStamp2 = datetime.datetime.now()
+    delta = tStamp2 - tStamp1
+    interval = str(int(delta.total_seconds() * 1000))
+    print ("completed in " + interval + " milliseconds")
 
-# # -----------------------------------Binary Search-------------------------------- #
+# # -----------------------------------Binary Search------------------------------------ #
 # implementation of a Binary Search
-# time complexity ( Order of Magnitude ) : O(1) (iterative method) O(log n) (recursive method)
+# time complexity: O(1) (iterative method) O(log n) (recursive method)
 def binarySearchWithPrinting(sortedList, n):
     def binarySearch(sortedList, n, x) :
         start = 0
@@ -59,18 +64,28 @@ def binarySearchWithPrinting(sortedList, n):
         return -1
 
     x = int(input("Enter the number to search: "))
+
+    tStamp1 = datetime.datetime.now()
+
     position = binarySearch(sortedList, n, x)
 
+    tStamp2 = datetime.datetime.now()
+    delta = tStamp2 - tStamp1
+    interval = str(int(delta.total_seconds() * 1000))
+
+    print ("completed in " + interval + " milliseconds")
     if (position != -1) :
         print("element number %d is present at position: %d" % (x, position))
     else :
         print("element number %d is not present in the list" % x)
 
-# --------------------------------------Bubble Sort----------------------------------- #
+# --------------------------------------Bubble Sort------------------------------------- #
 # program for implementing a Bubble Sort
+# time complexity: Avg, Worst: O(n^2) Best: O(n)
 
 def bubbleSortWithPrinting(arr, n):
     def bubbleSort(arr, n) :
+
         # Traverse through all array elements
         for i in range(n - 1) :
         # range(n) also work but outer loop will repeat one time more than necessary
@@ -80,37 +95,72 @@ def bubbleSortWithPrinting(arr, n):
                 # Swap if the element found is greater than the next element
                 if arr[j] > arr[j + 1] :
                     arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    tStamp1 = datetime.datetime.now()
     bubbleSort(arr, n)
+    tStamp2 = datetime.datetime.now()
+    delta = tStamp2 - tStamp1
+    interval = str(int(delta.total_seconds() * 1000))
+
     print ("the sorted array is:")
     for i in range(len(arr)) :
         print ("%d" %arr[i])
+    print ("completed in " + interval + " milliseconds")
     return arr
 
-# # -----------------------------------Selection Sort----------------------------------- #
-# # Routine for a Selection Sort
+# -----------------------------------Selection Sort------------------------------------- #
+# Routine for a Selection Sort
+# time complexity: Avg, Worst: O(n^2) Best: O(n)
+def selectionSortWithPrinting(arr, n):
+    tStamp1 = datetime.datetime.now()
+    for i in range(n) :
+        min_= i
+        for j in range(i + 1, n) :
+            if (arr[min_] > arr[j]) :
+                min_ = j
+        # swap performed
+        arr[i], arr[min_] = arr[min_], arr[i]
+    tStamp2 = datetime.datetime.now()
+    delta = tStamp2 - tStamp1
+    interval = str(int(delta.total_seconds() * 1000))
 
-# A = ['t','e','s','t','d','a','t','a']
 
-# for i in range(len(A)) :
-#     min_= i
-#     for j in range(i + 1, len(A)) :
-#         if (A[min_] > A[j]) :
-#             min_ = j
-#     # swap performed
-#     A[i], A[min_] = A[min_], A[i]
-
-# # main
-# for i in range(len(A)) :
-#    print (A[i])
+    print ("the sorted array is:")
+    for i in range(n) :
+        print ("%d" %arr[i])
+    print ("completed in " + interval + " milliseconds")
+    return arr
 
 # -----------------------------------Insertion Sort----------------------------------- #
+# Routine for a Insertion Sort
+# time complexity: Avg, Worst: O(n^2) Best: O(n)
+def insertionSortWithPrinting(arr, n):
+    tStamp1 = datetime.datetime.now()
+    for i in range(1, n) :
+        iEl = arr[i]
+        # move elements the greater than iEl one position ahead
+        j = i - 1
+        jEl = arr[j]
+        while j >= 0 and iEl < jEl :
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = iEl
+    tStamp2 = datetime.datetime.now()
+    delta = tStamp2 - tStamp1
+    interval = str(int(delta.total_seconds() * 1000))
 
+
+    print ("the sorted array is:")
+    for i in range(n) :
+        print ("%d" %arr[i])
+    print ("completed in " + interval + " milliseconds")
+    return arr
 
 # -------------------------------------Menu------------------------------------------- #
 # menu variables
 select = 0
 userList = []
-isSorted = 0
+isSorted = 2
 n = 0
 
 # -----------------------------Menu Helper------------------------------ #
@@ -147,10 +197,14 @@ def SelectRoutine() :
         SelectRoutine()
     elif (select == 4) :
         print ("Call the Selection Sort Routine")
-        #selectionSortWithPrinting(userList, n)
+        userList = selectionSortWithPrinting(userList, n)
+        isSorted = 1
+        SelectRoutine()
     elif (select == 5) :
         print ("Call the Insertion Sort Routine")
-        #insertionSortWithPrinting(userList, n)
+        userList = insertionSortWithPrinting(userList, n)
+        isSorted = 1
+        SelectRoutine()
     elif (select == 6) :
         print ("Input a new List!")
         listInput()
@@ -162,7 +216,7 @@ def listInput():
     global select, userList, isSorted, n
 
     isSorted = int(input("Enter 1 if your list is sorted and 0 if not: "))
-    if (isSorted != 1 & isSorted != 0) :
+    while (isSorted > 1 or isSorted < 0) :
         print("invalid selection")
         isSorted = int(input("Enter 1 if your list is sorted and 0 if not: "))
 
@@ -172,6 +226,10 @@ def listInput():
     # populate the list in sort entry order
     for i in range(n) :
         userList.append(int(input("Enter %dth element: " % i)))
+    # optionally pad with leading 0's to increase complexity
+    for i in range(10000) :
+        userList.insert(0, 0)
+    n += 10000
     SelectRoutine()
 
 listInput()
