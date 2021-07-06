bb504d2b2be8eae84e271bdde652112624c97b14
jbframe <framejb@gmail.com>
2021-07-06 17:42:37 -0600

    Lab 8 complete.

--- Labs/Lab8/MovieScript/MovieScript.py
+++ Labs/Lab8/MovieScript/MovieScript.py
@@ -3,6 +3,10 @@ class MovieScript(object) :
 
     # declare a data element as a list
     script = []
+    spyMovieKeyWords = ["enemy", "gadgets", "mission", "drone", "start", "space", "undercover", "disguise"]
+    segmentCharacters = []
+    isFirstUnitDirectors = []
+    segmentLength = []
 
     # the class constructor
     def __init__(self) :
@@ -11,11 +15,29 @@ class MovieScript(object) :
 
     # a class member function
     def getScript(self) :
-        return self.script
+        for x in range(len(self.script)) :
+            print (u"\u2022", self.script[x], end = "\n")
+            characters = ""
+            for y in range(len(self.segmentCharacters[x])) :
+                characters += self.segmentCharacters[x][y]
+                characters += ", "
+            characters = characters[:len(characters)-2]
+            print("\t featuring: " + characters)
+            length = str(self.segmentLength[x])
+            print("\t segment length: " + length + " minutes")
+            if self.isFirstUnitDirectors[x] == True :
+                print("\t First Unit Directors")
+            else :
+                print("\t Second Unit Directors")
+        print ("\n")
 
     # a class member function
-    def setScript(self, s) :
-        script = s
+    def setScript(self, script) :
+        for i in script :
+            self.script.append(i[0])
+            self.segmentCharacters.append(i[1])
+            self.segmentLength.append(i[2])
+            self.isFirstUnitDirectors.append(i[3])
 
     # a class member search function
     def search(self, seq) :
@@ -24,11 +46,16 @@ class MovieScript(object) :
         keyword = input().strip()
         isFound = False
         for i in range(len(seq)) :
-            if (keyword in seq[i]) :
+            if (keyword in seq[i][0]) :
                 isFound = True
                 print ("[keyword(s) found]")
                 print ("in scene sequence number ", i + 1)
-                print ("\"", seq[i], "\"")
+                print ("\"", seq[i][0], "\"")
+        for i in range(len(self.spyMovieKeyWords)) :
+            if (keyword in self.spyMovieKeyWords[i]) :
+                isFound = True
+                print ("[keyword(s) found]")
+                print ("in Spy Movie Keyword list, index ", i)
         if isFound == False :
             print ("[keyword(s) NOT found]")
         print ("\n")
--- Labs/Lab8/main.py
+++ Labs/Lab8/main.py
@@ -10,31 +10,33 @@ Course: CSC 242 Python Data Structures
 """)
 from MovieScript.MovieScript import MovieScript
 
+# the movie text, characters and sequences
+characters = ["Nick the bitcoin hacker", "Johnny the full stack drone engineer", "Lisa the psychologist", "ASIC the enemy AI"]
 
-# the movie text and sequences
 seq = []
-seq.append("opening teaser sequence")
-seq.append("main titles with theme song")
-seq.append("the plot unfolds")
-seq.append("meeting with the superiors")
-seq.append("the gadgets are issued")
-seq.append("the mission begins")
-seq.append("a romance ensues")
-seq.append("thwarted but persistent")
-seq.append("physical confrontation with the enemy")
-seq.append("the enemy is defeated")
-seq.append("the loose ends unfold")
-seq.append("on to the next mission")
+# the script is built using a sequence of segment stored in an array
+# each segment is an array with the following element [description, [characters], segment length, firstUnitDirectorsT/F]
+seq.append(["opening teaser sequence", [characters[0]], 3, True])
+seq.append(["main titles with theme song", [characters[0]], 4, True])
+seq.append(["the plot unfolds", [characters[0], characters[1], characters[3]], 6, True])
+seq.append(["meeting with the superiors", [characters[0], characters[1], characters[2]], 5, False])
+seq.append(["the gadgets are issued", [characters[0], characters[1], characters[2]], 5, False])
+seq.append(["the mission begins", [characters[0], characters[1], characters[2]], 8, True])
+seq.append(["a romance ensues", [characters[1], characters[2]], 6, True])
+seq.append(["subplot: the lead character starts a CS degree", [characters[2]], 3, False])
+seq.append(["thwarted but persistent", [characters[0], characters[1], characters[2], characters[3]], 7, True])
+seq.append(["subplot: the lead charter develops a quantum computer", [characters[2]], 4, True])
+seq.append(["physical confrontation with the enemy", [characters[0], characters[1], characters[2], characters[3]], 5, True])
+seq.append(["subplot: The quantum computer hacks into the enemy defences", [characters[2], characters[3]], 3, True])
+seq.append(["the enemy is defeated", [characters[0], characters[1], characters[2]], 5, True])
+seq.append(["the loose ends unfold", [characters[1], characters[2]], 4, False])
+seq.append(["on to the next mission", [characters[0], characters[1], characters[2]], 2, False])
 
-# convert the sequence list to a dictionary
 
 # create a new movie script object
 ms = MovieScript()
-ms.script = seq
-theScript = ms.getScript()
-for x in range(len(theScript)) :
-    print (u"\u2022", theScript[x], end = "\n")
-print ("\n")
 
+# the the object's methods!
+ms.setScript(seq)
+ms.getScript()
 ms.search(seq)
-
