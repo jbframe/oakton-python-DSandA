ee2f2851efc6d68f231c34cc0bcd406ffa4d2496
jbframe <framejb@gmail.com>
2021-07-13 22:19:29 -0600

    Added lab 10 boiler plate code.

---
+++ Labs/Lab10/Queue.py
@@ -0,0 +1,18 @@
+#  filename: Queue.py
+classQueue() :
+    # construct the list to implement the queue
+    def __init__(self) :
+        self.queue = []
+
+    # enqueue an element
+    def enqueue(self, item) :
+        self.queue.append(item)
+
+    # remove an element from the queue
+    def dequeue(self) :
+        if (len(self.queue) < 1) :
+          returnNonereturnself.queue.pop(0)
+
+    # display the current queue
+    def display(self) :
+        print(self.queue
