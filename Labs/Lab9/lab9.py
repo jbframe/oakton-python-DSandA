e6352ac0d6344b4f5e0667c5d36ca5c48e67dd98
jbframe <framejb@gmail.com>
2021-07-23 16:45:00 -0600

    Added CSV read.

--- Labs/Lab9/lab9.py
+++ Labs/Lab9/lab9.py
@@ -1,3 +1,6 @@
+from os import system
+system("clear")
+
 print("""
 --------------------------------Remarks------------------------------------
 Name: John Frame, Student Number: B02291550, Date: 7/10/2021
@@ -5,69 +8,46 @@ Lab # & Name: 9 - Stacks and The Writer's Block
 Course: CSC 242 Python Data Structures
 ---------------------------------------------------------------------------
 """)
-
-# # write data to a file
-# try :
-#     f = open("data.txt", "w", encoding = "utf-8")
-#     # perform file operations
-#     # first line
-#     f.write("the first line\n")
-#     # second line
-#     f.write("the second line\n")
-#     # third line
-#     f.write("the third line\n")
-# finally :
-#     f.close()
-
-# # read data from a file
-# try :
-#     data = []
-#     f = open("data.csv", "r", encoding = "utf-8")
-#     content = f.read().splitlines()
-#     # perform file operations
-#     for line in content :
-#         print (line)
-#         data.append(line)
-#         print (data)
-# finally :
-#     f.close()
-#     print (data)
-
+import csv
 import random
-# randomly select an item within a list of elements
-myList1 = ["me", "you", "him", "her", "it"]
-print (random.choice(myList1))
-print ("\n")
-# convert a list into a set ( to remove any duplicates )
-myList2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
-mySet1 = set(myList2)
-print (mySet1)
-print ("\n")
-print (sorted(mySet1))
-print ("\n")
-# randomly select a subset of elements
-samples = random.sample(mySet1, 6)
-print (samples)
-# sort the subset
-sortedList = sorted(samples)
-print ("\n")
-print (sortedList)
+
+# the Writer's Grid
+grid = []
+with open ('data.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        for i in row:
+            grid.append(i)
+
+
+# randomly select a subset of elements ( the stack )
+samples = random.sample(grid, 6)
+# print (samples)
+print ("[ the script ]")
+print("")
+print ("I started the day by looking for the", samples.pop(), end = ".")
+print ()
+print ("I planned later to walk to the", samples.pop(), end = ".")
+print ()
+print ("Surprisingly, I found the", samples.pop(), "was empty", end = ".")
+print ()
+print ("I wondered if a", samples.pop(), "would appear", end = ".")
+print ()
+print ("My aunt must have left my cellular telephone with the", samples.pop(), end = ".")
+print ()
+print ("Yesterday, I forgot to take the", samples.pop(), "to the meeting", end = ".")
 print ("\n\n")
 
- # list implementation of a Stack
-myStack = []
-for item in (10, 20, 30, 40) :
-    myStack.append(item)
-print("The elements in the stack.")
-print(myStack)
-print ("note: stack treated as a list")
-print (type(myStack))
-# elements that are popped
-# # can be tracked by printing them
-print("\nthe elements that were popped are:")
-print (myStack.pop())
-print (myStack.pop())
-print ("\nThe Stack after the elements are popped.")
-print (myStack)
-
-
+total_entropy = 0
+print ("\n")
+print ("please enter an integer 0-5 that will describe the entropy analysis")
+print ("for grid line 1")
+entropy = int(input())
+total_entropy += entropy
+print ("estimated entropy for grid line 1:", entropy)
+print ("for grid line 2")
+entropy = int(input())
+print ("estimated entropy for grid line 2:",  entropy)
+total_entropy += entropy
+print ("total entropy :",  total_entropy)
+print ("\n\n")
