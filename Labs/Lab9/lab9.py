3a4d80188cbe2b6904564ed9b5b569c16fd63894
jbframe <framejb@gmail.com>
2021-07-25 18:38:35 -0600

    Refectored to dynamically set the number of lines.

--- Labs/Lab9/lab9.py
+++ Labs/Lab9/lab9.py
@@ -21,25 +21,32 @@ with open ('data.csv', 'r') as csvfile:
 
 
 # randomly select a subset of elements ( the stack )
+numbeOfLines = 0
 samples = random.sample(grid, 6)
 print (samples, grid)
 print ("[ the script ]")
 print("")
 print ("I started the day by looking for the", samples.pop(), end = ".")
+numbeOfLines += 1
 print ()
 print ("I planned later to walk to the", samples.pop(), end = ".")
+numbeOfLines += 1
 print ()
 print ("Surprisingly, I found the", samples.pop(), "was empty", end = ".")
+numbeOfLines += 1
 print ()
 print ("I wondered if a", samples.pop(), "would appear", end = ".")
+numbeOfLines += 1
 print ()
 print ("My aunt must have left my cellular telephone with the", samples.pop(), end = ".")
+numbeOfLines += 1
 print ()
 print ("Yesterday, I forgot to take the", samples.pop(), "to the meeting", end = ".")
+numbeOfLines += 1
 print ("\n\n")
 
 total_entropy = 0
-numbeOfLines = 6
+
 
 print ("\n")
 print ("please enter an integer 0-5 that will describe the entropy analysis")
