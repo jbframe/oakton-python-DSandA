e5f5eddb76a822dd266991add6dac42434ee5246
jbframe <framejb@gmail.com>
2021-07-29 19:34:58 -0600

    Updated lab 10 into OOP structure.

---
+++ Labs/Lab10/Lab10Project/main.py
@@ -0,0 +1,15 @@
+from os import system
+system("clear")
+
+print("""
+--------------------------------Remarks------------------------------------
+Name: John Frame, Student Number: B02291550, Date: 7/10/2021
+Lab # & Name: 10 - Simulating a Customer Queue at the Local Drive - Through
+Course: CSC 242 Python Data Structures
+---------------------------------------------------------------------------
+""")
+from Queue.Queue import Queue
+
+queue = Queue()
+
+print(queue)
