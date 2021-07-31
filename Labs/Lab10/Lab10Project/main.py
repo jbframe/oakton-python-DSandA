47b500d0934081b65c79ceaf233debee7030b074
jbframe <framejb@gmail.com>
2021-07-30 19:38:57 -0600

    Updated main file for lab 10.

--- Labs/Lab10/Lab10Project/main.py
+++ Labs/Lab10/Lab10Project/main.py
@@ -10,6 +10,12 @@ Course: CSC 242 Python Data Structures
 """)
 from Queue.Queue import Queue
 
-queue = Queue()
-
-print(queue)
+# instantiate a class object
+q = Queue()
+# object uses methods of the class
+q.enqueue("Customer 1")
+q.enqueue("Doordash Customer 2")
+q.enqueue("GrubHub Customer 3")
+q.enqueue("Customer 4")
+q.dequeue()
+q.display()
